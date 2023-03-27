c17d5bcd18c2e1da303db8a08f43c16f40fdfb22
VinaySehwag14 <vinaysehwag14@gmail.com>
2023-03-27 13:59:11 +0530

    added Dimension API for getting different devices width

--- components/game/NumberContainer.jsx
+++ components/game/NumberContainer.jsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Dimensions } from "react-native";
 import React from "react";
 import Colors from "../../constants/colors";
 
@@ -12,12 +12,19 @@ const NumberContainer = ({ children }) => {
 
 export default NumberContainer;
 
+//* for getting device dimensions  window will give whole screen excluding status bar
+const deviceWidth = Dimensions.get("window").width;
+// const deviceHight = Dimensions.get("window").height;
+
+// console.log(deviceWidth, "this iswid🍺");
+// console.log(deviceHight, "this deviceHight🍺");
+
 const styles = StyleSheet.create({
   container: {
     borderWidth: 4,
     borderColor: Colors.accent500,
-    padding: 24,
-    margin: 24,
+    padding: deviceWidth < 380 ? 12 : 24,
+    margin: deviceWidth < 380 ? 12 : 24,
     borderRadius: 8,
     alignItems: "center",
     justifyContent: "center",
