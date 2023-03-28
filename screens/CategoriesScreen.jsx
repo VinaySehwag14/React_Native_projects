333f6fa93ada51064c59259ade6d020d239d034c
VinaySehwag14 <vinaysehwag14@gmail.com>
2023-03-28 18:31:31 +0530

    diplay data and styled the cards

---
+++ screens/CategoriesScreen.jsx
@@ -0,0 +1,25 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { CATEGORIES } from "../data/dummy-data";
+import { FlatList } from "react-native";
+import CategoryGridTile from "../components/CategoryGridTile";
+
+function renderCategoryItem(itemData) {
+  return (
+    <CategoryGridTile title={itemData.item.title} color={itemData.item.color} />
+  );
+}
+
+const CategoriesScreen = () => {
+  return (
+    <FlatList
+      data={CATEGORIES}
+      keyExtractor={(item) => item.id}
+      renderItem={renderCategoryItem}
+    />
+  );
+};
+
+export default CategoriesScreen;
+
+const styles = StyleSheet.create({});
