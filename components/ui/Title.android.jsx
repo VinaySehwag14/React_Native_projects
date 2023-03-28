5c6cdcf559827d536520dddb9957f5dde820509a
VinaySehwag14 <vinaysehwag14@gmail.com>
2023-03-28 11:43:48 +0530

    added different code files for different devices

--- components/ui/Title.android.jsx
+++ components/ui/Title.android.jsx
@@ -1,4 +1,4 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Platform } from "react-native";
 import React from "react";
 import Colors from "../../constants/colors";
 
---
+++ components/ui/Title.ios.jsx
@@ -0,0 +1,22 @@
+import { View, Text, StyleSheet, Platform } from "react-native";
+import React from "react";
+import Colors from "../../constants/colors";
+
+const Title = ({ children }) => {
+  return <Text style={styles.title}>{children}</Text>;
+};
+
+export default Title;
+
+const styles = StyleSheet.create({
+  title: {
+    fontFamily: "open-sans-bold",
+    fontSize: 24,
+    color: "white",
+    textAlign: "center",
+    borderColor: "white",
+    padding: 12,
+    maxWidth: "80%",
+    width: 300,
+  },
+});
