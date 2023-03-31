f3aa5a4dae81935b31ba7d923c099a830449cc8a
VinaySehwag14 <vinaysehwag14@gmail.com>
2023-03-31 13:18:45 +0530

    added dynamic data to diff slides using flatlist

--- screens/MealsOverviewScreen.jsx
+++ screens/MealsOverviewScreen.jsx
@@ -1,12 +1,26 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, FlatList, View } from "react-native";
 import React from "react";
 import { MEALS } from "../data/dummy-data";
+import MealItem from "../components/MealItem";
 
 const MealsOverviewScreen = ({ route }) => {
   const catId = route.params.categoryId;
+
+  const displayedMeals = MEALS.filter((mealItem) => {
+    return mealItem.categoryIds.indexOf(catId) >= 0;
+  });
+
+  function renderMealItem(itemData) {
+    return <MealItem title={itemData.item.title} />;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>MealsOverviewScreen-{catId}</Text>
+      <FlatList
+        data={displayedMeals}
+        keyExtractor={(item) => item.id}
+        renderItem={renderMealItem}
+      />
     </View>
   );
 };
