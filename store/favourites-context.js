8bf77048168614d1a9dc2ac2832b378e2376c615
VinaySehwag14 <vinaysehwag14@gmail.com>
2023-04-24 15:33:29 +0530

    added context

--- store/favourites-context.js
+++ store/favourites-context.js
@@ -1,13 +1,34 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
-const FavouriteContext = createContext({
+const FavoritesContext = createContext({
   ids: [],
   addFavorite: () => {},
   removeFavorite: (id) => {},
 });
 
 function FavoritesContextProvider({ children }) {
-  return <FavoritesContextProvider>{children}</FavoritesContextProvider>;
+  const [favoriteMealIds, setFavoriteMealIds] = useState([]);
+
+  function addFavorite(id) {
+    setFavoriteMealIds((currentFavIds) => [...currentFavIds, id]);
+  }
+
+  function removeFavorite(id) {
+    setFavoriteMealIds((currentFavIds) =>
+      currentFavIds.filter((mealId) => mealId !== id)
+    );
+  }
+
+  const value = {
+    ids: favoriteMealIds,
+    addFavorite: addFavorite,
+    removeFavorite: removeFavorite,
+  };
+  return (
+    <FavoritesContext.Provider value={value}>
+      {children}
+    </FavoritesContext.Provider>
+  );
 }
 
 export default FavoritesContextProvider;
