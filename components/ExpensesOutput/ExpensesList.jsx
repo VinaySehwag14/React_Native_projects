bd524dec5e8d910da5a4b07661dc197ff307a50f
VinaySehwag14 <vinaysehwag14@gmail.com>
2023-04-28 11:38:45 +0530

    added expenses output dummy data

--- components/ExpensesOutput/ExpensesList.jsx
+++ components/ExpensesOutput/ExpensesList.jsx
@@ -2,7 +2,7 @@ import { FlatList, StyleSheet, Text, View } from "react-native";
 import React from "react";
 
 const ExpensesList = () => {
-  return <FlatList></FlatList>;
+  return <FlatList />;
 };
 
 export default ExpensesList;
