e12db9896fe5992ade9585cbf6c3dcae745c07d7
VinaySehwag14 <vinaysehwag14@gmail.com>
2023-05-08 18:24:34 +0530

    added app wide state

--- screens/ManageExpenses.jsx
+++ screens/ManageExpenses.jsx
@@ -1,12 +1,14 @@
 import { StyleSheet, Text, View } from "react-native";
-import React, { useLayoutEffect } from "react";
+import React, { useContext, useLayoutEffect } from "react";
 import IconButton from "../components/UI/IconButton";
 import { GlobalStyles } from "../constants/styles";
 import Button from "../components/UI/Button";
+import { ExpensesContext } from "../store/expenses-context";
 
-const ManageExpenses = ({ route, navigation }) => {
+function ManageExpense({ route, navigation }) {
+  const expensesCtx = useContext(ExpensesContext);
+
   const editedExpenseId = route.params?.expenseId;
-
   const isEditing = !!editedExpenseId;
 
   useLayoutEffect(() => {
@@ -16,14 +18,31 @@ const ManageExpenses = ({ route, navigation }) => {
   }, [navigation, isEditing]);
 
   function deleteExpenseHandler() {
+    expensesCtx.deleteExpense(editedExpenseId);
     navigation.goBack();
   }
+
   function cancelHandler() {
     navigation.goBack();
   }
+
   function confirmHandler() {
+    if (isEditing) {
+      expensesCtx.updateExpense(editedExpenseId, {
+        description: "Test!!!!",
+        amount: 29.99,
+        date: new Date("2022-05-20"),
+      });
+    } else {
+      expensesCtx.addExpense({
+        description: "Test",
+        amount: 19.99,
+        date: new Date("2022-05-19"),
+      });
+    }
     navigation.goBack();
   }
+
   return (
     <View style={styles.container}>
       <View style={styles.buttons}>
@@ -46,9 +65,9 @@ const ManageExpenses = ({ route, navigation }) => {
       )}
     </View>
   );
-};
+}
 
-export default ManageExpenses;
+export default ManageExpense;
 
 const styles = StyleSheet.create({
   container: {
